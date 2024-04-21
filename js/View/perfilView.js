c8a5dea2bb3022d0f32926a1c8d48c78c89c4b78
pedro <pedro.gueirez@gmail.com>
2024-04-21 19:36:53 +0100

    123

--- js/View/perfilView.js
+++ js/View/perfilView.js
@@ -10,9 +10,10 @@ function render(perfis) {
                 //frase bonita
                 const frase = document.createElement("div");
                 frase.id = "frase";
-                frase.innerHTML = '<div class="frase1">FULL-STACK DEVELOPER</div>' +
-                '<div class="frase2">I can help your business to</div>' +
-                '<div class="frase3">Get better and grow fast</div>';
+                frase.innerHTML = '<div class="frase1">Hi, I\'m Pedro Gueirez </div>' +
+                '<div class="frase2">FULL-STACK DEVELOPER</div>' +
+                '<div class = "frase3"> I\'m a former captain of the management and administration ship who decided to navigate the seas of code. I\'ve dabbled in sales, led teams, and now, I\'m plunging into the programming ocean.</div>' +
+                '<div class = "frase3"> Whether steering projects or charming clients, I\'m always seeking the next frontier. Currently, I\'m navigating the tech universe, where every bug is a challenge to conquer, and every line of code is a small victory.</div>';
                 
                 apresentation.appendChild(frase);
     
@@ -26,19 +27,19 @@ function render(perfis) {
         const contactos = document.createElement("div");
         contactos.id = "contactos";
         contactos.innerHTML = `
-        <button type="button" class="btn btn-outline-success">Education</button>
-        <button type="button" class="btn btn-outline-success">Projects</button>`;
+        <button type="button" class="btn btn-outline-primary" id="contacto1">Hire Me</button>
+        <button type="button" class="btn btn-outline-primary" id="contacto2">Projects</button>`;
         container.appendChild(contactos);
 
         //About me
-        const aboutMe = document.createElement("div");
+       /*  const aboutMe = document.createElement("div");
         aboutMe.id = "aboutMe";
         aboutMe.innerHTML = '<div class = "aboutme1"> About me </div>' +
         '<div class = "aboutme2"> I\'m a former captain of the management and administration ship who decided to navigate the seas of code. I\'ve dabbled in sales, led teams, and now, I\'m plunging into the programming ocean.</div>' +
         '<div class = "aboutme2"> Whether steering projects or charming clients, I\'m always seeking the next frontier. Currently, I\'m navigating the tech universe, where every bug is a challenge to conquer, and every line of code is a small victory.</div>' +
         '<div class = "aboutme2"> Outside the coding sphere, catch me grooving to music, appreciating the wonders of nature, indulging in video games, and exploring all things tech-related.</div>' +
         '<div class = "aboutme2">Let\'s collaborate and create something amazing together.</div>';
-        container.appendChild(aboutMe);
+        container.appendChild(aboutMe); */
 
         //footer
         const footer = document.createElement("div");
